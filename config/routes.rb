cca758efc9d523787d9ad078591633064b994b3f
Paulinendemo <pauline.ndemo@student.moringaschool.com>
2022-11-23 14:15:22 +0300

    done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  
+  
+  get '/plants/:id', to: 'plants#show'
+  post '/plants', to: 'plants#create'
+  get '/plants', to: 'plants#index'
+  
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
